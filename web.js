6a8962274975fe633a03a2c578eac7ed5ee7f626
Rene Vallecillo <reneval@gmail.com>
2013-07-10 21:32:14 -0600

    missing encode type

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	data = fs.readFileSync('./index.html')
-  response.send(data.toString);
+  response.send(data.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
