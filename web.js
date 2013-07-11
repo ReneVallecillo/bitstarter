3bb3eed37b9ed02e702e9f5c9ce5495d3a592a7c
Rene Vallecillo <reneval@gmail.com>
2013-07-10 22:17:38 -0600

    adding suggestions from jshint

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	data = fs.readFileSync('./index.html')
+	data = fs.readFileSync('./index.html');
   response.send(data.toString('utf-8'));
 });
 
